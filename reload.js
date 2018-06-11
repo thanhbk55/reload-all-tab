c2e8d8156b9e6d709a50d02c219b50473cad7431
thanhbk55 <dinhduythanh51292@gmail.com>
2018-06-11 11:53:08 +0900

    add command

--- reload.js
+++ reload.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+function reload(){
   chrome.windows.getAll({populate:true}, (windows) => {
     windows.forEach((window) => {
       window.tabs.forEach((tab) => {
@@ -6,4 +6,14 @@ chrome.browserAction.onClicked.addListener((tab) => {
       })
     })
   })
+}
+
+chrome.browserAction.onClicked.addListener(() => {
+  reload()
+})
+
+chrome.commands.onCommand.addListener((command) => {
+  if(command === "reload-command"){
+    reload()
+  }
 })
